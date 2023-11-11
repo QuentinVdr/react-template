df4a30582dd282ab005ec2eccad39a59168fb435
Quentin VERDIER <quent1.verdier@gmail.com>
2023-11-11 02:52:23 +0100

    Update config files

--- template/src/contexts/ReactQueryProvider.jsx
+++ template/src/contexts/ReactQueryProvider.jsx
@@ -1,21 +1,31 @@
 import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import handleErrors from '@utils/errors/handleErrors';
-import { useState } from 'react';
+import { useMemo } from 'react';
+
+/** Define component props */
+ReactQueryProvider.propTypes = {
+  children: React.PropTypes
+};
+/** Setup handle error for all request */
+const mutationCache = new MutationCache({
+  onError: handleErrors
+});
+
+/** Setup handle error for all request */
+const queryCache = new QueryCache({
+  onError: handleErrors
+});
 
 /** Component used to handle the react query configuration and tools */
 export const ReactQueryProvider = ({ children }) => {
-  // Create a react query client to interact with a cache
-  const [queryClient] = useState(
+  const queryClient = useMemo(
     () =>
       new QueryClient({
-        mutationCache: new MutationCache({
-          onError: (error) => handleErrors(error)
-        }),
-        queryCache: new QueryCache({
-          onError: (error) => handleErrors(error)
-        })
-      })
+        mutationCache,
+        queryCache
+      }),
+    []
   );
 
   return (
