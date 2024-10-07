19ae3619082efd5108492923ab9aef30e2f75377
Quentin Verdier <quent1.verdier@gmail.com>
2024-10-07 10:55:58 +0200

    🏗️ Add Dockerfile and update project configuration for improved build and runtime setup

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,12 +1,31 @@
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+        api: 'modern'
+      }
+    }
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    watch: {
+      usePolling: true, // Enable polling instead of native file watching
+      interval: 100 // Optional: adjust the polling interval (in milliseconds)
+    }
+  },
   resolve: {
     // Registering alias paths
     alias: {
+      '@': '/src',
       '@api': '/src/api',
       '@assets': '/public/assets',
       '@components': '/src/components',
